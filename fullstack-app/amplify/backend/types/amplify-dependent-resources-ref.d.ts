1053f86a593b711bc7659046978b7db7472a1746
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-11-28 14:38:16 -0600

    push

---
+++ fullstack-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fullstackappb4dbd9bd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
